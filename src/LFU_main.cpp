0aa95e41f7be589def432cc9b5d46bee659d95be
DmitryS017 <X.x.xh@yandex.ru>
2023-09-24 23:04:53 +0300

    loading main files of LFU and Perfect caches

---
+++ src/LFU_main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+#include "../include/LFU_Cache.hpp"
+
+
+LFU::LFU_Page<int, int> slow_get_page(int key){
+    LFU::LFU_Page<int, int> Page {key, 1};
+    return Page;
+}
+
+
+int main(){
+    size_t buffer_size;
+    size_t input_size;
+    int elem_id;
+
+    std::cin >> buffer_size >> input_size;
+    assert(std::cin.good());
+
+    LFU::LFU_Cache<int, int> cache(buffer_size);
+
+    int hits = 0;
+    for(int i = 0; i < input_size; i++){
+        std::cin >> elem_id;
+        hits += cache.LookUpdate(elem_id, slow_get_page);
+    }
+
+    std::cout << "Hits:" << hits << '\n';
+}
---
+++ src/Perfect_main.cpp
@@ -0,0 +1,33 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "../include/Perfect_Cache.hpp"
+
+
+Perfect::Cache_Page<int, int> slow_get_page(int key, int next){
+    Perfect::Cache_Page<int, int> Page{key, next};
+    return Page;
+}
+
+
+int main(){
+    size_t buffer_size;
+    size_t input_size;
+    int elem_id;
+
+    std::cin >> buffer_size >> input_size;
+    assert(std::cin.good());
+
+    std::vector<int> input_buffer;
+
+    Perfect::GetPagesKeys(input_size, input_buffer);
+    Perfect::P_Cache<int, int> cache(buffer_size, input_size, input_buffer);
+
+    int hits = 0;
+    for(int i = 0; i < input_size; i++){
+        hits += cache.LookUpdate(input_buffer[i], slow_get_page);
+    }
+
+    std::cout << "Hits:" << hits << "\n";
+}
